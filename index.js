84f5327cfea241fd277e463bd482d5eb4ca3ec48
Chisato Nishikigi <110145061+lycorisor@users.noreply.github.com>
2022-08-12 00:04:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 exports.__esModule = true;
 var ms_rest_nodeauth_1 = require("@azure/ms-rest-nodeauth");
 (0, ms_rest_nodeauth_1.loginWithUsernamePasswordWithAuthResponse)(process.argv[2], process.argv[3]).then(function (authres) {
-    console.log(authres);
+    console.log(authres.subscriptions);
 })["catch"](function (err) {
     console.log("error");
 });
